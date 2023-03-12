217b4f428b256397c89107e48e144b9d48961f20
Fabowale Awwal <fabowalemuhawwal@gmail.com>
2023-03-12 04:02:45 +0100

    card design

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+html {
+  height: 100%;
+}
+
+body,
+main {
+  min-height: 100vh;
+  background-color: hsl(220, 46%, 83%);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data() {
+    return {
+      qr_title: 'Improve your front-end skills by building projects',
+      qr_sub: 'Scan the QR code to visit Frontend Mentor and take your coding skills to the next level'
+    }
+  }
+})
+</script>
+
+<template>
+  <main class="py-[100px]">
+    <div class="w-[90%] rounded-xl md:w-[300px] mx-auto p-[16px] bg-[#fff]">
+      <img src="/qrcode.png" class="rounded-xl" alt="" />
+      <div class="mt-[20px] space-y-[20px]">
+        <div class="mx-auto font-[700] font-[Outfit] text-[25px] md:text-[23px] title text-center">
+          {{ qr_title }}
+        </div>
+        <div
+          class="mx-auto w-[85%] font-[400] font-[Outfit] text-[17px] md:w-[95%] pb-[30px] md:text-[18px] sub text-center">
+          {{ qr_sub }}
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap');
+
+.title {
+  color: hsl(218, 44%, 22%);
+}
+
+.sub {
+  color: hsl(212, 45%, 89%);
+}
+</style>
